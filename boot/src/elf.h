af754e32659807617b1448d3a07c9b505904abeb
Colin Davidson <colrdavidson@gmail.com>
2015-03-03 13:26:32 -0500

    boot: Parse ELF.

---
+++ boot/src/elf.h
@@ -0,0 +1,36 @@
+#ifndef _ELF_H_
+#define _ELF_H_
+
+#include "efi.h"
+
+#define EI_NIDENT 16
+
+typedef struct {
+	u8  e_ident[EI_NIDENT];
+	u16 e_type;
+	u16 e_machine;
+	u32 e_version;
+	u64 e_entry;
+	u64 e_phoff;
+	u64 e_shoff;
+	u32 e_flags;
+	u16 e_ehsize;
+	u16 e_phentsize;
+	u16 e_phnum;
+	u16 e_shentsize;
+	u16 e_shnum;
+	u16 e_shstrndx;
+} Elf64_Ehdr;
+
+typedef struct {
+	u32 p_type;
+	u32 p_flags;
+	u64 p_offset;
+	u64 p_vaddr;
+	u64 p_paddr;
+	u64 p_filesz;
+	u64 p_memsz;
+	u64 p_align;
+} Elf64_Phdr;
+
+#endif // _ELF_H_
--- boot/src/main.c
+++ boot/src/main.c
@@ -1,5 +1,8 @@
 #include "efi.h"
+#include "elf.h"
 #include "io.h"
+
+#define KERNEL_MAX_LOAD_ADDR 0x400000000
 
 EfiSystemTable *ST;
 
@@ -19,6 +22,12 @@ void start(EfiHandle image_handle, EfiSystemTable *sys_table) {
 	u16 *path = u"\\kernel.elf";
     EfiFileInfo *kernel_info;
 	usize kernel_info_size = 0;
+	u64 kernel_addr = KERNEL_MAX_LOAD_ADDR;
+	char *kernel;
+	u32 i;
+	Elf64_Phdr *kernel_phdr;
+
+	Elf64_Ehdr *kernel_hdr;
 
 	ST = sys_table;
 
@@ -29,7 +38,6 @@ void start(EfiHandle image_handle, EfiSystemTable *sys_table) {
 	status = sfs->open_volume(sfs, &root);
 
 	status = root->open(root, &kernel_file, path, EFI_FILE_MODE_READ, 0);
-
 	printf("Open kernel: %d\n", (i32)status);
 
 	status = kernel_file->get_info(kernel_file, &FileInfoGUID, &kernel_info_size, NULL);
@@ -38,15 +46,36 @@ void start(EfiHandle image_handle, EfiSystemTable *sys_table) {
     sys_table->boot_services->allocate_pool(EfiLoaderData, kernel_info_size, (void**)&kernel_info);
 
 	status = kernel_file->get_info(kernel_file, &FileInfoGUID, &kernel_info_size, kernel_info);
-
 	printf("Kernel get info: %d\n", (i32)status);
-
 	printf("Kernel file size: %d\n", (i32)kernel_info->file_size);
+
+	status = sys_table->boot_services->allocate_pages(AllocateMaxAddress, EfiLoaderData, ((kernel_info->file_size+4095) & ~4095)/4096, &kernel_addr);
+	printf("kernel alloc: %d at 0x%lx\n", (i32)status, kernel_addr);
+
+	status = kernel_file->read(kernel_file, &kernel_info->file_size, (void*)kernel_addr);
+	printf("kernel read: %d\n", (i32)status);
+	status = kernel_file->close(kernel_file);
+	printf("kernel closed: %d\n", (i32)status);
+
+    kernel = (char*)kernel_addr;
+
+	if (kernel[0] == 0x7f && kernel[1] == 'E' && kernel[2] == 'L' && kernel[3] == 'F') {
+		printf("Looks like a valid kernel to me, y'know\n");
+		kernel_hdr = (Elf64_Ehdr*)kernel;
+		printf("Our entry is 0x%lx\n", kernel_hdr->e_entry);
+		printf("Our type is %d\n", (u32)kernel_hdr->e_type);
+		printf("There are %d PSections\n", kernel_hdr->e_phnum);
+		for (i = 0; i < kernel_hdr->e_phnum; ++i) {
+			kernel_phdr = (Elf64_Phdr*)(kernel + kernel_hdr->e_phoff + (kernel_hdr->e_phentsize * i));
+			printf("PSection with type 0x%x\n", kernel_phdr->p_type);
+		}
+	}
 
 	printf("Loaded at 0x%lx\n", (u64)lip->image_base);
 
 	printf("EFI Version %d.%02d\n", ST->hdr.revision >> 16, ST->hdr.revision & 0xffff);
 	printf("EFI Firmware: ");
+
 	sys_table->con_out->output_string(sys_table->con_out, sys_table->fw_vendor);
 	printf(" (rev %d.%02d)\n", sys_table->fw_revision >> 16, sys_table->fw_revision & 0xffff);
 
