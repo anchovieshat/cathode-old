8dde449147b6a37f2377036b1ed6811e1183b886
Jake Greenfield <xales@naveria.com>
2015-03-06 09:25:13 -0500

    kernel: panic support

--- kernel/src/lang.rs
+++ kernel/src/lang.rs
@@ -1,9 +1,18 @@
+use core::prelude::*;
+use core::fmt;
 
 #[lang="eh_personality"]
 fn eh_personality() { }
 
 #[lang="panic_fmt"]
-fn panic_fmt() -> ! { loop { } }
+fn panic_fmt(fmt: fmt::Arguments, file: &'static str, line: usize) -> ! {
+    println!("panic: {}", fmt);
+    loop {
+        unsafe {
+            asm!("hlt");
+        }
+    }
+}
 
 #[lang="stack_exhausted"]
 fn stack_exhausted() -> ! { loop { } }
