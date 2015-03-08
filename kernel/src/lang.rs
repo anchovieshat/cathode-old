43e9444e4eed1b8dc6502c290a20bff26a3e87b8
Jake Greenfield <xales@naveria.com>
2015-03-07 22:17:10 -0500

    Use file and line number in panic.

--- kernel/src/lang.rs
+++ kernel/src/lang.rs
@@ -6,7 +6,7 @@ pub fn eh_personality() { }
 
 #[lang="panic_fmt"]
 extern fn panic_fmt(fmt: fmt::Arguments, file: &'static str, line: usize) -> ! {
-    println!("panic: {}", fmt);
+    println!("panic: {} ({}:{})", fmt, file, line);
     loop {
         unsafe {
             asm!("hlt");
